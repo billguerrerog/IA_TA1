ba85a6a8608b4de86c76fdfc305a9b317820935f
Bill <zloop1995@gmail.com>
2022-04-24 01:17:15 -0500

    Hot fixes

--- Interfaz.py
+++ Interfaz.py
@@ -4,71 +4,70 @@ from math import e
 import random
 import numpy as np
 
-
 root = Tk()
-root.geometry("600x300")
+root.geometry("600x250")
+root.title("Simulated Annealing - Courier")
 
-root.title("Simulated Anneling")
-'''
-    Funciones 
-'''
-
+##Funciones
+#Generar una ruta
 def GenerarRuta(mapa):
-  n = len(mapa)
-  ruta = [None] * n
-  cont = 0
-  while True:   
-    if cont==n:
-      break
-    val = random.randint(0,n-1)
-    if val in ruta:
-      continue 
-    ruta[cont] = val
-    cont += 1
-  return ruta
+    n = len(mapa)
+    ruta = [None] * n
+    cont = 0
+    while True:
+        if cont==n:
+            break
+        val = random.randint(0, n-1)
+        if val in ruta:
+            continue
+        ruta[cont] = val
+        cont += 1
+    return ruta
 
-def calcular_costo_gasolina(mapa,SolActual):
-    costoxgalon = 16.17
-    galones = 9.25 / 100
+#Cálculo del tiempo, de acuerdo con la distancia total
+def CalcularTiempo(mapa,SolActual):
+    horaXkm = 1/40 #40km/h - 0.025 horas por kilómetro
     ruta = []
-    suma = 0
+    suma_distancia = 0
     for i in range(len(SolActual) - 1):
         ruta.append(mapa.item(SolActual[i], SolActual[i + 1]))
-        suma = suma + ruta[i]
-    costo_gasolina = galones * costoxgalon * suma
-    return costo_gasolina
+        suma_distancia = suma_distancia + ruta[i]
+    tiempo_total = horaXkm * suma_distancia
+    return tiempo_total
 
-
-def simulatedAnneling(mapa,rutaActual):
+#Implementación de Algoritmo Simulated Annealing
+def SimulatedAnnealing(mapa,rutaActual):
     solActual = rutaActual
     temp = 1000
-    Fenfri = 0.005
-    while temp>=0.0015:
+    enfriamiento = 0.005
+    while temp >= 0.0015:
         solNueva = GenerarRuta(mapa)
-        energiaActual = 0
-        energiaNueva = 0 
+        tiempo_actual = 0
+        tiempo_nuevo = 0
         for i in range(0,len(mapa)-1):
             distActual = mapa[solActual[i],solActual[i+1]]
             distNueva = mapa[solNueva[i],solNueva[i+1]]
-            energiaActual += distActual
-            energiaNueva += distNueva
-        #minimizar distancias
-        probCambio = 0.0000000000
-        if energiaNueva < energiaActual:
-            probCambio = 1.0000000000
+            tiempo_actual += distActual
+            tiempo_nuevo += distNueva
+
+        #Se minimiza la distancia
+        probabilidad_cambio = 0.0000000000
+        if tiempo_nuevo < tiempo_actual:
+            probabilidad_cambio = 1.0000000000
         else:
-            probCambio = (e)**((energiaActual-energiaNueva)/(temp)) 
-        ##DADO LA probabilidad podremos hacer el cambio
+            probabilidad_cambio = (e)**((tiempo_actual-tiempo_nuevo)/(temp))
+
+        #De acuerdo a la probabilidad se realiza el cambio o no
         coef = random.randint(0,100)
-        if coef/100 <= probCambio:
+        if coef/100 <= probabilidad_cambio:
             solActual = [] + solNueva
+
         #Bajar temperatura
-        temp = temp*(1-Fenfri)
+        temp = temp*(1-enfriamiento)
 
     return solActual  
 
-
-
+#Generar mapa de acuerdo con el número de puntos de entrega
 def GenerarMapa(num):
     n=int(num)
     mapa = []
@@ -86,17 +85,17 @@ def GenerarMapa(num):
         if i == j :
           mapa[i][j] = 0
         else:    
-          dist = random.randint(85,324)
+          dist = random.randint(5,50)
           mapa[i][j] = dist
           mapa[j][i] = dist
         i += 1
     return np.matrix(mapa)
 
-def matriz(Ciudad):
+def matriz(puntos_entrega):
     top = Toplevel()
-    top.title("Matriz Generado")
+    top.title("Matriz Generada")
     global mapa
-    mapa=GenerarMapa(Ciudad)
+    mapa=GenerarMapa(puntos_entrega)
 
     global rutaActual
     global rutaNueva
@@ -104,8 +103,8 @@ def matriz(Ciudad):
     rutaActual = generar_ruta_inicial(mapa)
     my_label= Label(top, text=mapa).pack()
 
-def matriz1(Ciudad):
-    mapa=GenerarMapa(Ciudad)
+def MatrizPrueba(puntos_entrega):
+    mapa=GenerarMapa(puntos_entrega)
     rutaActual,rutaNueva = [],[]
     rutaActual = generar_ruta_inicial(mapa)
     return mapa,rutaActual
@@ -116,51 +115,49 @@ def generar_ruta_inicial(mapa):
         rutaInicial.append(i)
     return rutaInicial
 
-def solucion1(mapa,ruta_act):
-    solucionFinal = simulatedAnneling(mapa,ruta_act)
-    rutaIn=generar_ruta_inicial(mapa)
-    costoInicial = calcular_costo_gasolina(mapa,rutaIn)
-    costoFinal = calcular_costo_gasolina(mapa,solucionFinal)
+def SolucionPrueba(mapa,ruta_act):
+    solucionFinal = SimulatedAnnealing(mapa,ruta_act)
     return solucionFinal
 
+# Vista Solución
 def solucion():
     top = Toplevel()
     top.title("Solución")
-    top.geometry("600x300")
-    solucionFinal = simulatedAnneling(mapa,rutaActual)
+    top.geometry("600x400")
+    solucionFinal = SimulatedAnnealing(mapa,rutaActual)
     rutaIn=generar_ruta_inicial(mapa)
-    costoInicial = calcular_costo_gasolina(mapa,rutaIn)
-    costoFinal = calcular_costo_gasolina(mapa,solucionFinal)
-    rutaIText=Label(top,text="Ruta Inicial: ",height=5,font=10)
-    rutaI=Label(top,text=rutaIn,height=5,font=10)
-    CIT=Label(top,text="Costo Inicial: ",height=5,font=10)
-    CI=Label(top,text=costoInicial,height=5,font=10)
-    rutaOT=Label(top,text="Ruta Optimizada: ",height=5,font=10)
-    rutaO=Label(top,text=solucionFinal,height=5,font=10)
-    COT=Label(top,text="Costo Optimizado: ",height=5,font=10)
-    CO=Label(top,text=costoFinal,height=5,font=10)
+    tiempo_inicial = CalcularTiempo(mapa,rutaIn)
+    costo_final = CalcularTiempo(mapa,solucionFinal)
+    rutaIText=Label(top,text="Ruta Inicial: ",height=3,font=10)
+    rutaI=Label(top,text=rutaIn,height=3,font=10)
+    CIT=Label(top,text="Tiempo Inicial: ",height=3,font=10)
+    CI=Label(top,text=tiempo_inicial,height=3,font=10)
+    espacio2 = Label(top, text="               ")
+    rutaOT=Label(top,text="Ruta Optimizada: ",height=3,font=10)
+    rutaO=Label(top,text=solucionFinal,height=3,font=10)
+    COT=Label(top,text="Tiempo Optimizado: ",height=3,font=10)
+    CO=Label(top,text=costo_final,height=3,font=10)
 
     rutaIText.grid(row=0,column=0)
     rutaI.grid(row=0,column=1)
     CIT.grid(row=1,column=0)
     CI.grid(row=1,column=1)
-    rutaOT.grid(row=0,column=2)
-    rutaO.grid(row=0,column=3)
-    COT.grid(row=1,column=2)
-    CO.grid(row=1,column=3)
+    espacio2.grid(row=2, column=0)
+    rutaOT.grid(row=3,column=0)
+    rutaO.grid(row=3,column=1)
+    COT.grid(row=4,column=0)
+    CO.grid(row=4,column=1)
     return solucionFinal
 
-
+#Vista Pruebas
 def Pruebas():
     top = Toplevel()
     top.title("Pruebas")
-    top.geometry("200x700")
+    top.geometry("250x300")
 
-    
     top1 = Toplevel()
     top1.title("Pruebas")
-    top1.geometry("600x600")
-
+    top1.geometry("500x250")
 
     # Create A main frame
     main_frame= Frame(top1)
@@ -186,58 +183,53 @@ def Pruebas():
     n = 10
     for i in range(n):
         n=i+1
-        mapa1,ruta_act=matriz1(5)
-        solucionActual=solucion1(mapa1,ruta_act)
-        if calcular_costo_gasolina(mapa1,generar_ruta_inicial(mapa1)) > calcular_costo_gasolina(mapa1,solucionActual):
+        mapa1,ruta_act=MatrizPrueba(5)
+        solucionActual=SolucionPrueba(mapa1,ruta_act)
+        if CalcularTiempo(mapa1,generar_ruta_inicial(mapa1)) > CalcularTiempo(mapa1,solucionActual):
             exito += 1
-        elif calcular_costo_gasolina(mapa1,generar_ruta_inicial(mapa1)) < calcular_costo_gasolina(mapa1,solucionActual):
+        elif CalcularTiempo(mapa1,generar_ruta_inicial(mapa1)) < CalcularTiempo(mapa1,solucionActual):
             fallas += 1
         else:
             iguales += 1
         Label(second_frame,text="Prueba ").grid(row=i,column=0)
         Label(second_frame,text=n).grid(row=i,column=1)
-        Label(second_frame,text="Costo Inicial: ").grid(row=i,column=2)
-        Label(second_frame,text=calcular_costo_gasolina(mapa1,generar_ruta_inicial(mapa1))).grid(row=i,column=3)
-        Label(second_frame,text="Costo Final: ").grid(row=i,column=4)
-        Label(second_frame,text=calcular_costo_gasolina(mapa1,solucionActual)).grid(row=i,column=5)
-    PET=Label(top,text="Pruebas exitosas: ",height=5,font=10)
-    PE=Label(top,text=exito,height=5,font=10)
-    PFT=Label(top,text="Pruebas fallidas: ",height=5,font=10)
-    PF=Label(top,text=fallas,height=5,font=10)
-    PIT=Label(top,text="Iguales: ",height=5,font=10)
-    PI=Label(top,text=iguales,height=5,font=10)
+        Label(second_frame,text="Tiempo Inicial: ").grid(row=i,column=2)
+        Label(second_frame,text=CalcularTiempo(mapa1,generar_ruta_inicial(mapa1))).grid(row=i,column=3)
+        Label(second_frame,text="Tiempo Final: ").grid(row=i,column=4)
+        Label(second_frame,text=CalcularTiempo(mapa1,solucionActual)).grid(row=i,column=5)
+
+    #Widgets & Grid System
+    PET=Label(top,text="Pruebas exitosas: ",height=3,font=10)
+    PE=Label(top,text=exito,height=3,font=10)
+    PFT=Label(top,text="Pruebas fallidas: ",height=3,font=10)
+    PF=Label(top,text=fallas,height=3,font=10)
+    PIT=Label(top,text="Sin cambio: ",height=3,font=10)
+    PI=Label(top,text=iguales,height=3,font=10)
     PET.grid(row = 0, column = 0)
-    PE.grid(row =1,column = 0)
-    PFT.grid(row =2,column = 0)
-    PF.grid(row =3,column = 0)
-    PIT.grid(row =4,column = 0)
-    PI.grid(row =5,column = 0)
+    PE.grid(row = 0,column = 1)
+    PFT.grid(row = 1,column = 0)
+    PF.grid(row = 1,column = 1)
+    PIT.grid(row = 2,column = 0)
+    PI.grid(row = 2,column = 1)
 
-
-'''
-    Creacion de widgets
-'''
-Ciudades = Label(root, text="N° Ciudades",height=5,font=10)
+##Vista principal
+#Widgets
+puntos_entrega = Label(root, text="N° Puntos de Entrega",height=3,font=10)
 espacio = Label(root, text="               ")
-numero_ciudad = Entry(root, width=40)
-Grafo_generado = Button(root, text="Ver matriz generado", command=lambda: matriz(numero_ciudad.get()))
-Solucion = Button(root, text="Ver Solucion",command=solucion)
-label = Label(root, text="5 Ciudades y 10 Casos de uso",height=5,font=10)
+numero_puntos_entrega = Entry(root, width=30)
+generar_matriz = Button(root, text="Generar Matriz", command=lambda: matriz(numero_puntos_entrega.get()))
+Solucion = Button(root, text="Ver Solución",command=solucion)
+label = Label(root, text="10 casos con 5 puntos de entrega",height=3,font=10)
 pruebas = Button(root, text="Ver Pruebas",command=Pruebas)
 
-
-
-'''
-    Sistema grid
-'''
-Ciudades.grid(row=0,column=2)
-numero_ciudad.grid(row=0, column=3)
-Grafo_generado.grid(row=1, column=2)
+#Grid System
+puntos_entrega.grid(row=0,column=2)
+numero_puntos_entrega.grid(row=0, column=3)
+generar_matriz.grid(row=1, column=2)
 Solucion.grid(row=1, column=3)
 espacio.grid(row=2, column=0)
 label.grid(row=3,column=2)
-pruebas.grid(row=4, column=2)
-
+pruebas.grid(row=3, column=3)
 
 root.mainloop()
 
